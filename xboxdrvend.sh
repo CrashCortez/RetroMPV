5f2cb8f4fedf3869751f95262850add127888133
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-10 11:05:34 -0800

    Update xboxdrvend.sh

--- xboxdrvend.sh
+++ xboxdrvend.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo killall xboxdrv > /dev/null 2>&1
+source /opt/retropie/configs/all/xboxdrvend.sh > /dev/null 2>&1
+sudo pkill -CONT mpg123 > /dev/null 2>&1
+fbset -g 320 240 320 240 8
+fbset -g 320 240 320 240 32
