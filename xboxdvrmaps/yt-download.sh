7e1dc4bfdcbe435780935b2cb7cfeb8a25243af4
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-10 12:56:55 -0800

    Create yt-download.sh

---
+++ xboxdvrmaps/yt-download.sh
@@ -0,0 +1,3 @@
+youtube-dl -f 137+140 --no-mtime --dateafter now-24hours -o /home/pi/RetroPie/roms/videos/"Newest Crash video.mp4" https://www.youtube.com/channel/UCwnTzGTRsNpen-2Nz38yGAQ
+
+https://www.youtube.com/channel/UCwnTzGTRsNpen-2Nz38yGAQ
