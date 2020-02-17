bd9b57a555de1db7eae4831c5dce680872457a5d
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-02-17 08:36:54 -0800

    Update control_updater_menu.sh

--- control_updater_menu.sh
+++ control_updater_menu.sh
@@ -251,19 +251,12 @@ function system_reboot() {
 
 function update_menu() {
     if validate_url https://raw.githubusercontent.com/CrashCortez/RetroMPV/master/control_updater_menu.sh; then
-        sudo mkdir -p ~/RetroPie/retropiemenu/Controllertools
-        sudo rm RetroMPV
-        git clone https://github.com/CrashCortez/RetroMPV.git
-        sudo chown pi:pi -R RetroMPV
-	cd
+        #sudo mkdir -p ~/RetroPie/retropiemenu/Controllertools
+        cd
         cd ~/RetroPie/retropiemenu/Controllertools
-	sudo rm control_updater_menu.sh
-	cd
-	cd RetroMPV
-        sudo chmod 777 control_updater_menu.sh
+        sudo wget -O control_updater_menu.sh https://raw.githubusercontent.com/CrashCortez/RetroMPV/master/control_updater_menu.sh
+        sudo chmod 775 control_updater_menu.sh
         sudo chmod a+x *.sh
-        sudo cp control_updater_menu.sh /home/pi/RetroPie/retropiemenu
-	cd 
         echo "---------------"
         echo "|| Success!  ||"
         echo "---------------"
